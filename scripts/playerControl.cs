9c7c61121dfae931565ffdac1447c8bff6030383
Aghin Shah <aghinsa@gmail.com>
2018-12-22 19:08:02 +0530

    roll a ball

---
+++ scripts/playerControl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerControl : MonoBehaviour {
+
+    private float moveHorizontal;
+    private float moveVertical;
+    private Rigidbody rg;
+    public float speed;
+    public int score;
+    public Text scoreText;
+	// Use this for initialization
+	void Start () 
+    {
+        rg = GetComponent<Rigidbody>();
+        score = 0;
+        updateScore();
+	 }
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+    {
+        moveVertical = Input.GetAxis("Vertical");
+        moveHorizontal= Input.GetAxis("Horizontal");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rg.AddForce(movement * speed);
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Coin"))
+            {
+            other.gameObject.SetActive(false);
+            score += 1;
+            updateScore();
+            }
+    }
+    void updateScore()
+    {
+        scoreText.text = "Score : " + score.ToString();
+
+
+    }
+}
